a87c91482b8b2f2afbf7d31493bcad2192b46e1e
opelooo <mathys.alberino@gmail.com>
2025-11-23 07:30:00 +0700

    Update [127] Adding Navigation (with React Navigation) & Configuring Navigation

---
+++ screens/ManageExpense.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function ManageExpenses() {
+  return (
+    <View>
+      <Text>ManageExpenses</Text>
+    </View>
+  );
+}
+
+export default ManageExpenses;
